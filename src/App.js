4638534ebec88b3b436c7baeea8d3a2e3d891ea2
Fidella1 <109987595+Fidella1@users.noreply.github.com>
2025-05-22 11:39:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-// src/App.js
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import BoardPage from './pages/BoardPage';
-import TaskPage from './pages/TaskPage';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BoardForm from "./components/Board/BoardForm";
+import BoardList from "./components/Board/BoardList";
+import BoardPage from "./pages/BoardPage";
 
 function App() {
   return (
     <Router>
-      <div className="app">
+      <div className="App">
+        <h1 className="heading">Task Manager</h1>
         <Routes>
-          <Route path="/" element={<BoardPage />} />
-          <Route path="/boards/:boardId" element={<TaskPage />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <BoardForm onCreate={() => window.location.reload()} />
+                <BoardList />
+              </>
+            }
+          />
+          <Route path="/boards/:id" element={<BoardPage />} />
         </Routes>
       </div>
     </Router>
